1a17b6b7bc1fed72fa7f81235959d26252516e47
Camila <yagimaj@gmail.com>
2026-05-04 12:45:15 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+// Camila Aki Yagima Jeronimo
+package dobackofront;
+public class Main {
+
+
+    public static void imprimeNovaMensagem(String texto) {
+        System.out.println("Meu primeiro programa com funcoes e parametro: " + texto);
+    }
+
+    public static void main(String[] args) {
+
+       
+        imprimeNovaMensagem("Olá");
+        imprimeNovaMensagem("Camila");
+        imprimeNovaMensagem("Java");
+    }
+}
